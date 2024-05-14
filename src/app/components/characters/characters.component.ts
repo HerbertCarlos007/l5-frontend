54925836bcbb6a365717669feeb7d3bf767cf92c
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-05-14 17:14:35 -0300

    Renderizando personagens

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CharacterService } from '../../services/character.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-characters',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ],
+  providers: [CharacterService],
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.css'
 })
 export class CharactersComponent {
+  
+  
+  characters: any[] = [];
+  
 
+  constructor(private characterService: CharacterService) { }
+
+  ngOnInit(): void {
+    this.characterService.getCharacters().subscribe(characters => {
+      this.characters = characters;
+      console.log(this.characters)
+    });
+  }
 }
